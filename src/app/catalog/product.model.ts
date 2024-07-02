48e0a48402a096d6ca36491a89c5e48229436125
Rahmatullah Zadran <Rahmatullah.Zadran@TLH.LOCAL>
2024-07-02 12:51:07 +0100

    added array

---
+++ src/app/catalog/product.model.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imageName: string;
+    category: string;
+    discount: number;
+}
+
